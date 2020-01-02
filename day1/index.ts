271aca1f4b7f1432b70c289e35f0e8b31f778668
Meichen Zhou <jfmaggie@gmail.com>
2020-01-02 15:07:59 -0800

    Clean up + 1

--- day1/index.ts
+++ day1/index.ts
@@ -6,10 +6,28 @@ function getFuel(mass: number): number {
   return _.floor(mass / 3) - 2;
 }
 
+console.log("==== getFuel: ");
 console.log("12 -> 2, ", getFuel(12));
 console.log("14 -> 2, ", getFuel(14));
 console.log("1969 -> 654, ", getFuel(1969));
 console.log("100756 -> 33583, ", getFuel(100756));
+console.log("\n");
+
+function getTotalFuel(mass: number): number {
+  let total = 0;
+  let currentMass = getFuel(mass);
+  while (currentMass > 0) {
+    total += currentMass;
+    currentMass = getFuel(currentMass);
+  }
+  return total;
+}
+
+console.log("==== getTotalFuel: ");
+console.log("14 -> 2, ", getTotalFuel(14));
+console.log("1969 -> 966, ", getTotalFuel(1969));
+console.log("100756 -> 50346, ", getTotalFuel(100756));
+console.log("\n");
 
 const input = fs
   .readFileSync(__dirname + "/input.txt")
@@ -18,26 +36,17 @@ const input = fs
   .map(_.toNumber);
 
 // Part 1
-const output1 = input
+const output1 = _.chain(input)
   .map(getFuel)
-  .reduce((prevValue, currentValue) => prevValue + currentValue, 0);
+  .sum()
+  .value();
 
 console.log("expect part 1 output: 3295424, ", output1);
 
 // Part 2
-const moduleFuels = input.map(mass => {
-  let total = 0;
-  let currentMass = getFuel(mass);
-  while (currentMass > 0) {
-    total += currentMass;
-    currentMass = getFuel(currentMass);
-  }
-  return total;
-});
-
-const output2 = moduleFuels.reduce(
-  (prevValue, currentValue) => prevValue + currentValue,
-  0
-);
+const output2 = _.chain(input)
+  .map(getTotalFuel)
+  .sum()
+  .value();
 
 console.log("expect output2: 4940279, ", output2);
